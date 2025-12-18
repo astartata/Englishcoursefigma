a4713e842fed50f9dc6b884ada67bff739494521
astartata <astarta.ta@gmail.com>
2025-12-18 18:00:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  // корректно для GitHub Pages + Figma Make
-  base: './',
+  // ВАЖНО: Название вашего репозитория
+  base: '/Englishcoursefigma/',
 
   plugins: [react()],
-
   resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
     target: 'esnext',
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+    open: true,
   },
 })
