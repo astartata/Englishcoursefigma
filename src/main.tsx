8066e191b54bc837f4f3a3324621af04c8da892b
astartata <astarta.ta@gmail.com>
2025-12-18 18:12:04 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './app/App'
 import './styles/index.css'
 
-// Включаем темную тему для Tailwind
+// Включаем темную тему
 document.documentElement.classList.add('dark')
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
