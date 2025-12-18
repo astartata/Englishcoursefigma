762660c3fe0ecb0f11d04c06a6d619e583ca20a0
astartata <astarta.ta@gmail.com>
2025-12-18 18:18:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  // Убедитесь, что название репозитория в ссылке совпадает с этим:
+  // Проверьте, что имя репозитория в ссылке совпадает с этим:
   base: '/Englishcoursefigma/',
   plugins: [react()],
   resolve: {
